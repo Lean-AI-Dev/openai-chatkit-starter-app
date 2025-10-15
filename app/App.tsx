d5308060f74638eb6cf4f14529110f61aeb2a8eb
LeanIA <cordiez.consulting@gmail.com>
2025-10-15 18:22:24 +0200

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -3,7 +3,7 @@
 import { useCallback } from "react";
 import { ChatKitPanel, type FactAction } from "@/components/ChatKitPanel";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { Image } from "next/image";
+
 
 
 export default function App() {
