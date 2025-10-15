c6a1e90d1fa15b80032ca2fecc0ee8fe0adc4f33
LeanIA <cordiez.consulting@gmail.com>
2025-10-15 19:07:31 +0200

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -3,6 +3,8 @@
 import { useCallback } from "react";
 import { ChatKitPanel, type FactAction } from "@/components/ChatKitPanel";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { ChatKit } from "@openai/chatkit";
+import { options } from "./chatkit.config";
 
 
 
